ceb101ef8ea38a5d362fe365167b57c759a4ee15
yauhen sampir <yauhen_sampir@epam.com>
2015-01-27 13:13:19 +0300

    add windows phone 8.1 project
    stable

---
+++ App3/Infrastructure/Train.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace App3
+{
+    public class Train
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string City { get; set; }
+        public string TotalTime { get; set; }
+        public string Price { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string Description { get; set; }
+        public string TrainNote { get; set; }
+        public string Place { get; set; }
+        public Train(String[] train)
+        {
+            this.City = train[0];
+            this.TotalTime = train[4];
+            this.StartTime = train[2];
+            this.EndTime = train[3];
+            this.TrainNote = train[5];
+            this.Place = train[6];
+            this.Price = train[7];
+            this.Description = train[1];
+            this.To = train[8];
+            this.From = train[9];    
+        }
+    }
+}
---
+++ App3/Infrastructure/TrainGrabber.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Windows.Networking.NetworkOperators;
+
+namespace App3.Infrastructure
+{
+    class TrainGrabber
+    {
+        private static IEnumerable<Match> ParseTrainData(string data)
+        {
+            const string Pattern = "(?<city>&nbsp;&mdash; (.+?)</\\/?)|" +
+                                   "(?<trainDescription>train_description\">(.+?)<\\/?)|"+
+                                   "(?<startTime>start-time\">([^<]*)<\\/?)|" +
+                                   "(?<endTime>end-time\">(.+?)<\\/?)|" +
+                                   "(?<totalTime>time-total\">(.+?)<\\/?)|" +
+                                   "(?<trainNote><li class=\"train_note\">(.+?)<\\/?)|" +
+                                   "(?<place>car_type=.?\">([^<]*)<\\/?)|" +
+                                   "(?<trainPrice>\'price\':(.+?) /?)";
+            Regex rgx = new Regex(Pattern, RegexOptions.Singleline);
+            var match=rgx.Matches(data).Cast<Match>();
+            return match as Match[] ?? match.ToArray();
+        }
+
+        private static string GetUrl(string fromName, string toName,string date)
+        {
+            return "http://rasp.rw.by/ru/route/?from=" + fromName + "&to=" + toName + "&date=" + date;
+        }
+        private static string GetHtmlCode(string url)
+        {
+            var httpClient = new HttpClient();
+            var httpResponseMessage = httpClient.GetAsync(url).Result;
+            Stream res = httpResponseMessage.Content.ReadAsStreamAsync().Result;
+            StreamReader reader = new StreamReader(res, Encoding.UTF8);
+            return reader.ReadToEnd();
+        }
+
+        public static List<Train> GetTrainSchedure(string from, string to, string date)
+        {
+            var match = ParseTrainData(GetHtmlCode(GetUrl(from, to, date)));
+            var matches = match as IList<Match> ?? match.ToList();
+            int i = 1;
+            String[] train = new String[10];
+            train[9] = from;
+            train[8] = to;
+            List<Train> trainList = new List<Train>();
+            foreach (var m in match.Skip(2)){
+                if (i == 9)
+                {
+                    trainList.Add(new Train(train));
+                    i = 1;
+                }
+                    train[i - 1] = m.Groups[i].Value;
+                    i++;
+            }
+            return trainList;
+        }
+    }
+}
