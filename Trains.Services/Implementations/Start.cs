084ae7ef9deaf50231e4a0fe2ba80bc6af3b5355
yauhen sampir <yauhen_sampir@epam.com>
2015-03-30 11:20:07 +0300

    fix usings

--- Trains.Services/Implementations/Start.cs
+++ Trains.Services/Implementations/Start.cs
@@ -1,10 +1,8 @@
 ﻿using System.Threading.Tasks;
 using Windows.ApplicationModel.Resources;
-using Windows.Globalization;
 using Trains.Model.Entities;
 using Trains.Services.Interfaces;
 using Trains.Services.Tools;
-using Language = Trains.Model.Entities.Language;
 
 namespace Trains.Services.Implementations
 {
