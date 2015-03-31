2a87e69c2afe0a1ad7a16ce4672adc3d44248e70
Sergei Bokatuk <sbakatsiuk@gmail.com>
2015-03-31 15:29:29 +0300

    initial mvvmcross

---
+++ Trains.WP/Views/FirstView.xaml.cs
@@ -0,0 +1,12 @@
+using Cirrious.MvvmCross.WindowsCommon.Views;
+
+namespace Trains.WP.Views
+{
+    public sealed partial class FirstView : MvxWindowsPage
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
