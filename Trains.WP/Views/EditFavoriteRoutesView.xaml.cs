550cfb3c76bfa3121b3fd1d3a39163ed10e2396f
yauhen sampir <yauhen_sampir@epam.com>
2015-04-01 19:20:54 +0300

    fix [name]View.cs

--- Trains.WP/Views/EditFavoriteRoutesView.xaml.cs
+++ Trains.WP/Views/EditFavoriteRoutesView.xaml.cs
@@ -20,7 +20,7 @@ namespace Trains.WP.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class EditFavoriteRoutesView : Page
+    public sealed partial class EditFavoriteRoutesView
     {
         public EditFavoriteRoutesView()
         {
--- Trains.WP/Views/SettingsView.xaml.cs
+++ Trains.WP/Views/SettingsView.xaml.cs
@@ -20,7 +20,7 @@ namespace Trains.WP.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class SettingsView : Page
+    public sealed partial class SettingsView
     {
         public SettingsView()
         {
