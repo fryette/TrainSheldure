a67061c76da0ca8367e6a6a19b64c4796a59fd42
yauhen sampir <yauhen_sampir@epam.com>
2015-04-02 15:06:48 +0300

    add StopPointViewModel

---
+++ Trains.Core/ViewModels/StopPointViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Cirrious.MvvmCross.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Trains.Model.Entities;
+
+namespace Trains.Core.ViewModels
+{
+    public class StopPointViewModel : MvxViewModel
+    {
+        #region readonlyProperties
+        /// <summary>
+        /// Used to display stop points and time of arrival and time of departure.
+        /// </summary> 
+        public IEnumerable<TrainStop> Parameter { get; set; }
+        #endregion
+
+        #region command
+
+        public IMvxCommand GoToHelpPageCommand { get; private set; }
+
+        #endregion
+
+        #region actions
+
+        public StopPointViewModel(string param)
+        {
+            Parameter = JsonConvert.DeserializeObject<List<TrainStop>>(param);
+            GoToHelpPageCommand = new MvxCommand(GoToHelpPage);
+        }
+
+        /// <summary>
+        /// Go to saved by user routes page.
+        /// </summary>
+        private void GoToHelpPage()
+        {
+            ShowViewModel<HelpViewModel>();
+        }
+
+        #endregion
+    }
+}
