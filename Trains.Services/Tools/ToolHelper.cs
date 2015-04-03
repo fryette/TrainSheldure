a51f654ccd1d203b490c431851edd7a1b7db6eb1
yauhen sampir <yauhen_sampir@epam.com>
2015-04-03 18:49:00 +0300

    delete ToolHelper in WP

--- Trains.Services/Tools/ToolHelper.cs
+++ Trains.Services/Tools/ToolHelper.cs
@@ -1,23 +1,11 @@
 ﻿using System;
 using System.Globalization;
-//using Windows.UI.Popups;
 using Trains.Model.Entities;
 
 namespace Trains.Services.Tools
 {
     public static class ToolHelper
     {
-
-        public static void ShowMessageBox(string message)
-        {
-            //Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync
-            //    (Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
-            //    {
-            //        var dialog = new MessageDialog(message);
-            //        await dialog.ShowAsync();
-            //    });
-
-        }
         public static string GetDate(DateTimeOffset datum, string selectedVariantOfSearch = null)
         {
             if (selectedVariantOfSearch == "На все дни") return "everyday";
