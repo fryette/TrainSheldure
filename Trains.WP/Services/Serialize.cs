932ee49b562ddf9186e68500b0135edc570e7dc7
Evgeniy Pakalo <evgeniy.pakalo@gmail.com>
2015-04-07 13:24:39 +0300

    [!] added using for wp

--- Trains.WP/Services/Serialize.cs
+++ Trains.WP/Services/Serialize.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+using Trains.Infrastructure.Interfaces;
 using Trains.Services.Interfaces;
 using Windows.Storage;
 
