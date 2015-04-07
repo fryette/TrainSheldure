738c703e8853f5a7a9a9dffffb92eb89e01dfc84
yauhen sampir <yauhen_sampir@epam.com>
2015-04-07 17:42:25 +0300

    delete obsolete Constants

--- Trains.Resources/Constants.cs
+++ Trains.Resources/Constants.cs
@@ -11,8 +11,6 @@ namespace Trains.Resources
         public const string CurrentLanguage = "currentLanguage";
         public const string FavoriteRequests = "favoriteRequests";
         public const string LastTrainList = "LastTrainList";
-        public const string IsSecondStart = "isSecondStart";
-        public const string IsFirstStart = "isFirstStart";
     }
 }
 
