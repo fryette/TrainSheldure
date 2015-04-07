a98ee3b71950c4f09b2e9c85b1c413edf6ca7146
yauhen sampir <yauhen_sampir@epam.com>
2015-04-07 16:47:38 +0300

    move ISerializableService to Services

--- Trains.Services/Interfaces/ISerializableService.cs
+++ Trains.Services/Interfaces/ISerializableService.cs
@@ -2,7 +2,7 @@
 using System.Threading.Tasks;
 using Trains.Model.Entities;
 
-namespace Trains.Infrastructure.Interfaces
+namespace Trains.Services.Interfaces
 {
     public interface ISerializableService
     {
