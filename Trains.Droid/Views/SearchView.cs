c68ef35abd0e5dc087cb20cd20966f41f0069bfb
Evgeniy Pakalo <evgeniy.pakalo@gmail.com>
2015-04-09 14:27:45 +0300

    [*][android] added datePicker

--- Trains.Droid/Views/SearchView.cs
+++ Trains.Droid/Views/SearchView.cs
@@ -16,10 +16,45 @@ namespace Trains.Droid.Views
 	[Activity(Label = "SearchView")]
 	public class SearchView : MvxActivity
 	{
+		private Button _searchDateButton;
+		private DateTime _date;
+
 		protected override void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
 			SetContentView(Resource.Layout.SearchView);
+
+			_date = DateTime.Now;
+
+			_searchDateButton = FindViewById<Button>(Resource.Id.SearchDate);
+			_searchDateButton.Click += searchDateButton_Click;
 		}
+
+		private void searchDateButton_Click(object sender, EventArgs e)
+		{
+			ShowDialog((int)DialogTypes.DatePicker);
+		}
+
+		protected override Dialog OnCreateDialog(int id)
+		{
+			switch (id)
+			{ 
+				case (int)DialogTypes.DatePicker:
+					return new DatePickerDialog(this, HandleSearchDateSet, _date.Year, _date.Month, _date.Day);
+				default:
+					return base.OnCreateDialog(id);
+			}
+		}
+
+		private void HandleSearchDateSet(object sender, DatePickerDialog.DateSetEventArgs e)
+		{
+			_date = e.Date;
+			_searchDateButton.Text = _date.ToString();
+		}
+	}
+
+	enum DialogTypes : byte
+	{ 
+		DatePicker = 0	
 	}
 }
