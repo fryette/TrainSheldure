0c614dee6a1b048e6881a8da4843d37970995240
yauhen sampir <yauhen_sampir@epam.com>
2015-04-16 22:57:32 +0300

    add grouping

---
+++ Trains.WP/Converter/ListViewConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trains.Entities;
+using Windows.UI.Xaml.Data;
+
+namespace Trains.WP.Converter
+{
+   public class ListViewConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            CollectionViewSource collection = new CollectionViewSource();
+            collection.Source = ((List<Train>)value).GroupBy(x=>x.BeforeDepartureTime);
+            collection.IsSourceGrouped = true;
+            return collection;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
