469ae2ce6f618b7438f00a1e5dd75f527c87b75d
yauhen sampir <yauhen_sampir@epam.com>
2015-04-27 10:49:14 +0300

    fix loading resources

--- Trains.Resources/LocalData.cs
+++ Trains.Resources/LocalData.cs
@@ -11,14 +11,12 @@ namespace Trains.Resources
         private async Task<string> LoadContent(string fileName, string lang)
         {
             var assembly = typeof(Constants).GetTypeInfo().Assembly;
-            var name = string.Format("Trains.Resources.DataModels.{0}", fileName);
+            var name = string.Format("Trains.Resources.DataModels.{0}.{1}",lang, fileName);
             var stream = assembly.GetManifestResourceStream(name);
 
             if (stream == null) return null;
             using (var reader = new StreamReader(stream))
-            {
                 return await reader.ReadToEndAsync();
-            }
         }
 
         public async Task<T> GetData<T>(string fileName, string lang) where T : class
