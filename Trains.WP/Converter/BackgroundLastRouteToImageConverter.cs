95504b8dcdd32ad6deef100ae1fe00d5d93b7f70
yauhen sampir <yauhen_sampir@epam.com>
2015-05-04 12:57:56 +0300

    add new images(No favorite and No history)

---
+++ Trains.WP/Converter/BackgroundLastRouteToImageConverter.cs
@@ -0,0 +1,38 @@
+﻿using Cirrious.CrossCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trains.Core.Interfaces;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Trains.WP.Converter
+{
+    public class BackgroundLastRouteToImageConverter : IValueConverter
+    {
+        static readonly Dictionary<string, Uri> Pictures =
+   new Dictionary<string, Uri>()
+            {
+                {"ru",new Uri(@"ms-appx:///Assets/backgrounds/Dlya_Otobrazhenia.png")},
+                {"be",new Uri(@"ms-appx:///Assets/backgrounds/Dlya_Adlyustravannya.png")},
+                {"en",new Uri(@"ms-appx:///Assets/backgrounds/No_history.png")},
+            };
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return new ImageBrush
+            {
+                ImageSource = new BitmapImage() { UriSource = Pictures[Mvx.Resolve<IAppSettings>().Language.Id] },
+                Stretch = Stretch.UniformToFill
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Trains.WP/Converter/BackgroundRoutesToImageConverter.cs
@@ -0,0 +1,38 @@
+﻿using Cirrious.CrossCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trains.Core.Interfaces;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Trains.WP.Converter
+{
+    public class BackgroundRoutesToImageConverter : IValueConverter
+    {
+        static readonly Dictionary<string, Uri> Pictures =
+   new Dictionary<string, Uri>()
+            {
+                {"ru",new Uri(@"ms-appx:///Assets/backgrounds/Marshrutov_Ne.png")},
+                {"be",new Uri(@"ms-appx:///Assets/backgrounds/Marshrutau_Ne.png")},
+                {"en",new Uri(@"ms-appx:///Assets/backgrounds/No_favorites.png")},
+            };
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return new ImageBrush
+            {
+                ImageSource = new BitmapImage() { UriSource = Pictures[Mvx.Resolve<IAppSettings>().Language.Id] },
+                Stretch = Stretch.UniformToFill
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
