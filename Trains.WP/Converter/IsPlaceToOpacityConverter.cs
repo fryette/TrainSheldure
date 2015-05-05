f684846573ac50d8b959d576379ee2a0e4bd6a3d
yauhen sampir <yauhen_sampir@epam.com>
2015-05-05 19:46:22 +0300

    fix ScheduleView

--- Trains.WP/Converter/IsPlaceToOpacityConverter.cs
+++ Trains.WP/Converter/IsPlaceToOpacityConverter.cs
@@ -1,17 +1,19 @@
 ﻿using System;
-using Windows.UI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Media;
 
 namespace Trains.WP.Converter
 {
-    public class IsPlaceToColorConverter : IValueConverter
+    public class IsPlaceToOpacityConverter : IValueConverter
     {
+
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            if (value != null)
-                return ((string)value).Contains(":") ? new SolidColorBrush(Colors.Green) : new SolidColorBrush(Colors.Red);
-            return new SolidColorBrush(Colors.White);
+            return ((string)value).Contains(':') ? 1 : 0.5;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
