f12154d29a3f95a42033985a2a0766190108e536
yauhen sampir <yauhen_sampir@epam.com>
2015-05-06 16:19:47 +0300

    fix resources in ShareView

--- Trains.Core/ViewModels/ShareViewModel.cs
+++ Trains.Core/ViewModels/ShareViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Cirrious.MvvmCross.ViewModels;
 using Trains.Model.Entities;
+using Trains.Core.Resources;
 
 namespace Trains.Core.ViewModels
 {
@@ -13,13 +14,14 @@ namespace Trains.Core.ViewModels
 
         #region UIproperties
 
-        public string ShareHeader { get; set; }
-        public string ShareTitle { get; set; }
+        public string Header { get; set; }
+        public string Title { get; set; }
 
         #endregion
         public IEnumerable<ShareSocial> ShareItems { get; set; }
         public void Init()
         {
+            RestoreUIBinding();
             ShareItems = new List<ShareSocial>
             {
                 ShareSocial.Vkontakte,
@@ -29,6 +31,12 @@ namespace Trains.Core.ViewModels
                 ShareSocial.LinkedIn,
                 ShareSocial.Odnoklassniki
             };
+        }
+
+        private void RestoreUIBinding()
+        {
+            Header = ResourceLoader.Instance.Resource["SocialNetworks"];
+            Title = ResourceLoader.Instance.Resource["SupportUs"];
         }
     }
 }
