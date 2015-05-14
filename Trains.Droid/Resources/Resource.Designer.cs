5ee45e701bb4c83ed1a2cf6fa062f2c0bed71f2d
yauhen sampir <yauhen_sampir@epam.com>
2015-05-14 15:23:27 +0300

    add to mainview lastroute

--- Trains.Droid/Resources/Resource.Designer.cs
+++ Trains.Droid/Resources/Resource.Designer.cs
@@ -155,19 +155,22 @@ namespace Trains.Droid
 			public const int ItemView = 2130903044;
 			
 			// aapt resource value: 0x7f030005
-			public const int MainView = 2130903045;
+			public const int lastroutetemplate = 2130903045;
 			
 			// aapt resource value: 0x7f030006
-			public const int scheduletemplate = 2130903046;
+			public const int MainView = 2130903046;
 			
 			// aapt resource value: 0x7f030007
-			public const int ScheduleView = 2130903047;
+			public const int scheduletemplate = 2130903047;
 			
 			// aapt resource value: 0x7f030008
-			public const int SettingsView = 2130903048;
+			public const int ScheduleView = 2130903048;
 			
 			// aapt resource value: 0x7f030009
-			public const int SplashScreen = 2130903049;
+			public const int SettingsView = 2130903049;
+			
+			// aapt resource value: 0x7f03000a
+			public const int SplashScreen = 2130903050;
 			
 			static Layout()
 			{
