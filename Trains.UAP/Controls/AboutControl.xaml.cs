304a0cdadb101e34d1fbc4e4eec1f25126545cd2
Yauhen Sampir <yauhen_sampir@epam.com>
2015-07-13 12:30:53 +0300

    fix: Views

--- Trains.UAP/Controls/AboutControl.xaml.cs
+++ Trains.UAP/Controls/AboutControl.xaml.cs
@@ -12,6 +12,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Trains.Core.ViewModels;
+using Trains.Model.Entities;
+using Trains.UAP.Views;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +28,21 @@ namespace Trains.UAP.Controls
         public AboutControl()
         {
             this.InitializeComponent();
+        }
+
+        Dictionary<AboutPicture, Type> AboutItemsActions = new Dictionary<AboutPicture, Type>
+            {
+            {AboutPicture.AboutApp,typeof(AboutView)},
+            {AboutPicture.Settings,typeof(SettingsView)},
+            {AboutPicture.Share,typeof(ShareView)}
+            };
+
+        private void ListViewBase_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            var item = AboutItemsActions[((About) e.ClickedItem).Item];
+            if (item != null) Frame.Navigate(item);
+            else
+                ((MainViewModel) DataContext).TappedAboutItemCommand.Execute(e.ClickedItem);
         }
     }
 }
