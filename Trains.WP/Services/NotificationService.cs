050afe25e2757a1cb8ab5a8376f5974044d66fa0
Yauhen Sampir <Yauhen_Sampir@epam.com>
2015-08-12 19:55:17 +0300

    Add NotificationService

---
+++ Trains.WP/Services/NotificationService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Appointments;
+using Trains.Core.Interfaces;
+using Trains.Entities;
+
+namespace Trains.WP.Services
+{
+	public class NotificationService : INotificationService
+	{
+		private AppointmentCalendar _currentAppCalendar;
+
+		public async Task AddTrainToNotification(Train train)
+		{
+			//if (_currentAppCalendar == null)
+			//{
+			//	var appointmentStore = await AppointmentManager.RequestStoreAsync(AppointmentStoreAccessType.AppCalendarsReadWrite);
+			//	_currentAppCalendar = (await appointmentStore.FindAppointmentCalendarsAsync(FindAppointmentCalendarsOptions.IncludeHidden))[0];
+			//}
+
+			//var newAppointment = new Appointment
+			//{
+			//	Subject = train.City,
+			//	StartTime = DateTimeOffset.Parse(train.StartTime),
+			//	Duration = TimeSpan.FromSeconds(10),
+			//	Reminder = TimeSpan.FromMinutes(10),
+			//	Location = "Minsk",
+			//	RoamingId = "TestId"
+			//};
+			
+			////save appointment to calendar
+			//await _currentAppCalendar.SaveAppointmentAsync(newAppointment);
+		}
+	}
+}
