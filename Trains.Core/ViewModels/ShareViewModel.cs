b3b0fb8f5004b475a004ec7a9e8cc27bb4fb3c5c
Yauhen Sampir <Yauhen_Sampir@epam.com>
2015-08-24 14:11:25 +0300

    Create new model

--- Trains.Core/ViewModels/ShareViewModel.cs
+++ Trains.Core/ViewModels/ShareViewModel.cs
@@ -5,34 +5,57 @@ using Trains.Model.Entities;
 
 namespace Trains.Core.ViewModels
 {
-	public class ShareViewModel : MvxViewModel
-	{
+    public class ShareViewModel : MvxViewModel
+    {
 
-		#region UIproperties
+        #region UIproperties
 
-		public string Header { get; set; }
-		public string Title { get; set; }
+        public string Header { get; set; }
+        public string Title { get; set; }
 
-		#endregion
-		public IEnumerable<ShareSocial> ShareItems { get; set; }
-		public void Init()
-		{
-			RestoreUiBinding();
-			ShareItems = new List<ShareSocial>
-			{
-				ShareSocial.Vkontakte,
-				ShareSocial.Facebook,
-				ShareSocial.Twitter,
-				ShareSocial.GooglePlus,
-				ShareSocial.LinkedIn,
-				ShareSocial.Odnoklassniki
-			};
-		}
+        #endregion
+        public IEnumerable<ShareSocial> ShareItems { get; set; }
 
-		private void RestoreUiBinding()
-		{
-			Header = ResourceLoader.Instance.Resource["SocialNetworks"];
-			Title = ResourceLoader.Instance.Resource["SupportUs"];
-		}
-	}
+        private List<ImageFeature> _imageFeatures;
+        public List<ImageFeature> ImageFeatures
+        {
+            get { return _imageFeatures; }
+            set
+            {
+                _imageFeatures = value;
+                RaisePropertyChanged(() => ImageFeatures);
+
+            }
+        }
+
+        public void Init()
+        {
+            RestoreUiBinding();
+            ShareItems = new List<ShareSocial>
+            {
+                ShareSocial.Vkontakte,
+                ShareSocial.Facebook,
+                ShareSocial.Twitter,
+                ShareSocial.GooglePlus,
+                ShareSocial.LinkedIn,
+                ShareSocial.Odnoklassniki
+            };
+
+            ImageFeatures = new List<ImageFeature>
+            {
+                new ImageFeature {Path = "ms-appx:///Assets/Screenshots/1.png",Description = "New Featuer/1"},
+                new ImageFeature {Path = "ms-appx:///Assets/Screenshots/2.png",Description = "New Featuer/2"},
+                new ImageFeature {Path = "ms-appx:///Assets/Screenshots/3.png",Description = "New Featuer/3"},
+                new ImageFeature {Path = "ms-appx:///Assets/Screenshots/4.png",Description = "New Featuer/4"},
+                new ImageFeature {Path = "ms-appx:///Assets/Screenshots/5.png",Description = "New Featuer/5"},
+                new ImageFeature {Path = "ms-appx:///Assets/Screenshots/6.png",Description = "New Featuer/6"},
+            };
+        }
+
+        private void RestoreUiBinding()
+        {
+            Header = ResourceLoader.Instance.Resource["SocialNetworks"];
+            Title = ResourceLoader.Instance.Resource["SupportUs"];
+        }
+    }
 }
