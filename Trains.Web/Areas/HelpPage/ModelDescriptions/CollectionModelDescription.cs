be22507368c79007671be7eeb6212868041a6e80
Dzmitry_Rybalko <Dzmitry_Rybalko@epam.com>
2015-10-02 14:05:54 +0300

    Add wwwroot folder

--- Trains.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Trains.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
